181029dbdc04b69e6d1a755ee311dfa103854405
neel-scalent <neel@scalent.io>
2021-12-04 01:14:44 +0530

    fix: Sub func implementation fixed

--- libtest.go
+++ libtest.go
@@ -1,9 +1,9 @@
 package gophercart
 
-func Add(num1, num2 int32) (result int64) {
+func Add(num1, num2 int32) int64 {
 	return int64(num1 + num2)
 }
 
-func Sub(num1, num2 int32){
+func Sub(num1, num2 int32) int64 {
 	return int64(num1 - num2)
 }
